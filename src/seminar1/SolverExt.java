1749b2c0d5695aaaba5faef9bb1af2f8d4c21fbe
IlyaScorp <ilyabelan7cc@mail.ru>
2017-11-28 21:33:28 +0300

    Add files via upload

--- src/seminar1/SolverExt.java
+++ src/seminar1/SolverExt.java
@@ -1,8 +1,9 @@
-package seminar1;
+package Tasks;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 /**
  * ( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) ) = 101
@@ -20,7 +21,8 @@ import java.io.InputStreamReader;
  * Считаем, что операции деления на ноль отсутствуют
  */
 public class SolverExt {
-
+    //Криво, но работает
+    // пусть будет так.
     private static final String QUIT = "q";
 
     private static final char LEFT_PAREN   = '(';
@@ -30,19 +32,113 @@ public class SolverExt {
     private static final char TIMES        = '*';
     private static final char DIVISION     = '/';
 
-    private static double evaluate(String[] values) {
-        /* TODO: implement it */
-        return 0D;
+    private static double evaluate(String values) {
+        String[] arr = infToPostfic(values).split(" ");
+        int tmp;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            switch (arr[i]){
+                case "*": stack.push(stack.pop() * stack.pop());
+                    break;
+
+                case "+": stack.push(stack.pop() + stack.pop());
+                    break;
+
+                case "-": stack.push(-(stack.pop() - stack.pop()));
+                    break;
+
+                case "/": tmp = stack.pop(); stack.push(stack.pop() / tmp);
+                    break;
+
+                default: stack.push(Integer.parseInt(arr[i]));
+                    break;
+            }
+
+        }
+
+        return stack.pop();
     }
 
     public static void main(String[] args) {
         try (BufferedReader lineReader = new BufferedReader(new InputStreamReader(System.in))) {
             String sequence;
             while (!QUIT.equals(sequence = lineReader.readLine())) {
-                System.out.println(evaluate(sequence.split(" ")));
+                System.out.println(evaluate(sequence));
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static String infToPostfic(String str){
+        Stack<Character> stack = new Stack<>();
+        String[] arr = str.split(" ");
+        EasyMap<Character,Integer> prior = new EasyMap<>();
+        prior.put('*',3);
+        prior.put('/',3);
+        prior.put('+',2);
+        prior.put('-',2);
+        prior.put('(',1);
+        StringBuilder postfiks = new StringBuilder();
+        char tmp;
+        for (int i = 0; i < arr.length; i++) {
+            tmp = arr[i].charAt(0);
+            if ( 48 <= tmp && tmp <= 57){
+                postfiks.append(arr[i]).append(' ');
+            }else if (tmp == '('){
+                stack.push(tmp);
+            }else if (tmp == ')'){
+                tmp = stack.pop();
+                while (tmp != '('){
+                    postfiks.append(tmp).append(' ');
+                    tmp = stack.pop();
+                }
+            }else {
+                while (!stack.isEmpty() && (prior.get(stack.peek()) >= prior.get(tmp))){
+                    postfiks.append(stack.pop()).append(' ');
+                }
+                stack.push(tmp);
+            }
+
+        }
+        while (!stack.isEmpty()){
+            postfiks.append(stack.pop()).append(' ');
+        }
+        return postfiks.toString();
+    }
+
+
+
+
+
+    static class EasyMap<K,V>{
+
+        private K[] arrKey = null;
+        private V[] arrValue = null;
+        private int size = 0;
+
+        @SuppressWarnings("unchecked")
+        EasyMap(){
+            arrKey = (K[]) new Object[10];
+            arrValue = (V[]) new Object[10];
+        }
+
+        void put(K key, V value){
+            arrKey[size] = key;
+            arrValue[size++] = value;
+        }
+
+        V get(K key){
+            return arrValue[indexOf(key)];
+        }
+
+        private int indexOf(K key){
+            for (int i = 0; i < arrValue.length; i++) {
+                if (key == arrKey[i]){
+                    return  i;
+                }
+            }
+            return 0;
         }
     }
 }
